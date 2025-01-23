fbd16c01d347f4144de0ae0c6a59aa6701cfe540
JoshAskew <asksprit3@gmail.com>
2025-01-23 15:41:19 -0600

    added avatars

--- src/components/Tagline.tsx
+++ src/components/Tagline.tsx
@@ -5,20 +5,23 @@ import Logo from '../assets/Fatum-TDJB Logo.svg'
 import Avatar from '@mui/material/Avatar';
 import AvatarGroup from '@mui/material/AvatarGroup';
 // import Rating from '@mui/material/Rating';
+import Nadia from '../assets/nadia.png'
+import Dillon from '../assets/dillon.png'
+import Claire from '../assets/claire.png'
 
 
 function Tagline() {
   return (
     <div className='hero'>
       <img src={Logo} alt="DJ Blueprint Logo" className="logo" />
-      <h1 className="tagline-h1"> Amplify Your Music Career<br/>by&nbsp;&nbsp;
+      <h1 className="tagline-h1"> Amplify Your Music Career<br />by&nbsp;&nbsp;
         <span className="highlight-span">Mastering</span>&nbsp; Instagram
       </h1>
       <div className='tagline-container'>
-      <h5 className='tagline-h5'>Learn the exact Instagram strategy that helped 500+ artists build powerful personal brands and unlock opportunities they never thought possible (even if you're starting from zero)</h5>
+        <h5 className='tagline-h5'>Learn the exact Instagram strategy that helped 500+ artists build powerful personal brands and unlock opportunities they never thought possible (even if you're starting from zero)</h5>
       </div>
       <PrimaryBtnBlue className='cta-1' rightIcon={ArrowRight} text='Scale my music career' />
-      <br/>
+      <br />
       <div className="video-container">
         <iframe className='video-container'
           width="100%"
@@ -30,15 +33,15 @@ function Tagline() {
         ></iframe>
       </div>
       <div className='avatar-container'>
-      {/* <Rating className= 'rating' name="read-only" value={5} readOnly /> */}
-      <AvatarGroup spacing="small">
-  <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-  <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
-  <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
-  <div className='avatar-text'>Join 70,000+ followers who are already growing their careers
+        {/* <Rating className= 'rating' name="read-only" value={5} readOnly /> */}
+        <AvatarGroup spacing="small">
+          <Avatar alt="Remy Sharp" src={Nadia} />
+          <Avatar alt="Travis Howard" src={Claire} />
+          <Avatar alt="Cindy Baker" src={Dillon} />
+          <div className='avatar-text'>Join 70,000+ followers who are already growing their careers
 
-  </div>
-</AvatarGroup>
+          </div>
+        </AvatarGroup>
       </div>
     </div>
   );
